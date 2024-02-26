698795131c11b0c9013c119405562c4768a4025d
root <root@rocky.pdsea.f5net.com>
2024-02-25 20:22:28 -0800

    simple netlify test

--- src/components/App.js
+++ src/components/App.js
@@ -42,7 +42,7 @@ export default function App() {
     <div>
       <p>
         {format(addDays(new Date(), 1), 'EEEE d-MMM')} there will be{' '}
-        <strong>{results.length}</strong> near misses
+        <strong>{results.length}</strong> fly pigs
       </p>
       <hr></hr>
       {results
